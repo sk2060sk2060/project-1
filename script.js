3a9e89aa081661f7a13d9956915507b967566e80
Ryan Compton <ryan.wade.compton@gmail.com>
2020-01-28 20:43:26 -0700

    Added script for top 10 tracks

--- script.js
+++ script.js
@@ -3,18 +3,36 @@ var authorizationToken;
 var queryURL;
 var searchTerm;
 
-getToken();
+$(document).ready(function () {
 
+    requestToken();
+    getToken();
+
+});
+
+function requestToken() {
+
+    var redirectUri = window.location.href;
+    if (redirectUri.includes("#access_token=")) return;
+
+    if (redirectUri !== "https://sk2060sk2060.github.io/project-1" || redirectUri !== "http://127.0.0.1:8080/") {
+        redirectUri = "http://127.0.0.1:8080/"
+
+        var scope = "user-library-modify";
+        window.location.href = "https://accounts.spotify.com/authorize?client_id=21b6e99dfa9948818d67377855f4d685&response_type=token&scope=" + encodeURIComponent(scope) + "&redirect_uri=" + redirectUri;
+    }
+}
 function getToken() {
     var returnAuthorizationToken = location.hash.substr(1);
     authorizationToken = "Bearer " + returnAuthorizationToken.substring(returnAuthorizationToken.indexOf("=") + 1, returnAuthorizationToken.indexOf("&"));
     console.log(authorizationToken);
+
 }
 
 function buildQueryURL() {
     searchTerm = $("#search-term").val();
     console.log(searchTerm);
-    queryURL = "https://api.spotify.com/v1/search?q=" + searchTerm + "&type=artists";
+    queryURL = "https://api.spotify.com/v1/search?q=" + searchTerm + "&type=artist";
 }
 
 function searchForArtist() {
@@ -25,28 +43,41 @@ function searchForArtist() {
         //     "Authorization": authorizationToken
         // },
         beforeSend: function (request) {
-          request.setRequestHeader("Authorization", authorizationToken);
-          request.setRequestHeader("Accept", "application/json");
+            request.setRequestHeader("Authorization", authorizationToken);
+            request.setRequestHeader("Accept", "application/json");
         },
         url: queryURL,
-      }).fail(function (jqXHR, textStatus, errorThrown) {
-      }).done(function (response) {
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+    }).done(function (response) {
         console.log(response);
+        var firstSongID = response.artists.items[0].id;
+        queryURL = `https://api.spotify.com/v1/artists/${firstSongID}/top-tracks?country=US`;
+
+        $.ajax({
+            method: "GET",
+            // headers: {
+            //     "Authorization": authorizationToken
+            // },
+            beforeSend: function (request) {
+                request.setRequestHeader("Authorization", authorizationToken);
+                request.setRequestHeader("Accept", "application/json");
+            },
+            url: queryURL,
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+        }).done(function (response) {
+            console.log("tracks", response);
+            var artist = response[0].artists[0].name;
+            var title = response[0].name;
+            var musixMatchQuery = `${artist} - ${title}`;
+
+        });
+
     });
 }
 
-button.addEventListener("click", function () {
-    console.log("click");
-    
-    var redirectUri = window.location.href;
-    if (redirectUri !== "https://sk2060sk2060.github.io/project-1" || redirectUri !== "http://127.0.0.1:8080/") {
-        console.log(redirectUri);
-        var scope = "user-read-email";
-        window.location.href = "https://accounts.spotify.com/authorize?client_id=21b6e99dfa9948818d67377855f4d685&response_type=token&scope=" + encodeURIComponent(scope) + "&redirect_uri=" + redirectUri;
-    }
-});
 
-document.getElementById("search").addEventListener("click", function() {
+
+document.getElementById("search").addEventListener("click", function () {
     searchForArtist();
 });
 
